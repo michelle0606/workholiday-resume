c790e42076df9cb5f082bffbbd2cc14dd06d01b8
walle <michelle210181@gmail.com>
2019-05-23 01:15:37 +0800

    RWD for 425px

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const about = document.getElementById('link-about')
 const experience = document.getElementById('link-experience')
 const reason = document.getElementById('link-reason')
+const email = document.getElementById('email')
 
 about.onclick = e => {
   about.classList.add('active')
@@ -18,4 +19,8 @@ reason.onclick = e => {
   about.classList.remove('active')
   experience.classList.remove('active')
   reason.classList.add('active')
+}
+
+email.onclick = e => {
+  window.open('mailto:michelle210181@gmail.com')
 }
